d04c200a1e8a40cb9482b1b5e1ada93b599ca8ea
knightrishi <arnav.ara05@gmail.com>
2026-06-19 11:44:42 +0530

    Crawler logic done using jsoup

---
+++ src/main/java/com/arnav/distributedsearchengine/crawler/CrawlerService.java
@@ -0,0 +1,31 @@
+package com.arnav.distributedsearchengine.crawler;
+
+import com.arnav.distributedsearchengine.entity.Document;
+import com.arnav.distributedsearchengine.repository.DocumentRepository;
+import org.jsoup.Jsoup;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+@Service
+public class CrawlerService {
+    private final DocumentRepository repository;
+        public CrawlerService(DocumentRepository documentRepository) {
+            this.repository = documentRepository;
+        }
+        public Document crawl(String url) throws Exception{
+            org.jsoup.nodes.Document page = Jsoup.connect(url).get();
+            //the above line downloads the webpage.
+
+            Document document=new Document();
+            document.setUrl(url);
+            document.setTitle(page.title());
+            document.setHtmlContent(page.html());
+            document.setStatus("Crawled");
+            document.setCrawledAt(LocalDateTime.now());
+
+            return repository.save(document);
+
+
+        }
+
+}
