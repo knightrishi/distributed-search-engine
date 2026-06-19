83590e679d3330f3de84c34ecc28dc9652538318
knightrishi <arnav.ara05@gmail.com>
2026-06-19 20:55:21 +0530

    comments added for docs

--- src/main/java/com/arnav/distributedsearchengine/controller/CrawlController.java
+++ src/main/java/com/arnav/distributedsearchengine/controller/CrawlController.java
@@ -4,6 +4,8 @@ import com.arnav.distributedsearchengine.crawler.CrawlerService;
 import com.arnav.distributedsearchengine.entity.Document;
 import org.springframework.web.bind.annotation.*;
 
+
+//This is for crawling exactly one page not more than 1 like visit a page and store the required data
 @RestController
 @RequestMapping("/crawl")
 public class CrawlController {
